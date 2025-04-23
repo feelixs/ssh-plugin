52fab1f97ed8519d6ebebc21a9e67267d9c68b5d
Michael Felix <michael@felixcreations.com>
2025-04-23 13:29:07 -0400

    setup for java

---
+++ src/main/java/sshplugin/SshInitiator.java
@@ -0,0 +1,7 @@
+package sshplugin;
+
+import org.jetbrains.annotations.NotNull;
+
+public class SshInitiator implements ... {
+    // this will be where the action to initiate the ssh to the IDE terminal goes
+}
